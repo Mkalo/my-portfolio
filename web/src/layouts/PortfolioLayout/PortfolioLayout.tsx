9674ad60d4a8b586f2e3f8e50b99f14b797af284
Matheus Belo <matheus_belo@id.uff.br>
2022-08-07 06:30:04 -0300

    Create header and footer components and add them to layout

--- web/src/layouts/PortfolioLayout/PortfolioLayout.tsx
+++ web/src/layouts/PortfolioLayout/PortfolioLayout.tsx
@@ -1,9 +1,18 @@
+import Footer from 'src/components/Footer/Footer';
+import Header from 'src/components/Header/Header';
+
 type PortfolioLayoutProps = {
   children?: React.ReactNode;
 };
 
 const PortfolioLayout = ({ children }: PortfolioLayoutProps) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default PortfolioLayout;
