fff2ff008623cec8c6de1f52b022555ca3aec265
Matheus Belo <matheus_belo@id.uff.br>
2022-08-07 08:39:36 -0300

    Test auto deploy

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <h2>{'Footer'}</h2>
+      <h2>{'Test'}</h2>
       <p>{'Find me in ./web/src/components/Footer/Footer.tsx'}</p>
     </div>
   );
