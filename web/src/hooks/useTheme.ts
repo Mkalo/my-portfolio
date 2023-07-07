1a4aad3b129515f0b9eb2fc9655f974aa42a41bc
Matheus Belo <matheus_belo@id.uff.br>
2023-07-07 04:55:11 -0300

    Fix hook to work with prerender

--- web/src/hooks/useTheme.ts
+++ web/src/hooks/useTheme.ts
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 
+import { useIsBrowser } from '@redwoodjs/prerender/browserUtils';
+
 export function useTheme() {
-  const [theme, setTheme] = useState<'dark' | 'light'>(
-    document.documentElement.classList.contains('dark') ? 'dark' : 'light'
-  );
+  const browser = useIsBrowser();
+  if (browser) {
+    const [theme, setTheme] = useState<'dark' | 'light'>(
+      document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+    );
 
-  const toggleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    document.documentElement.classList.remove(theme);
-    document.documentElement.classList.add(newTheme);
-  };
+    const toggleTheme = () => {
+      const newTheme = theme === 'dark' ? 'light' : 'dark';
+      setTheme(newTheme);
+      localStorage.setItem('theme', newTheme);
+      document.documentElement.classList.remove(theme);
+      document.documentElement.classList.add(newTheme);
+    };
 
-  return { theme, toggleTheme };
+    return { theme, toggleTheme };
+  } else {
+    return { theme: 'light', toggleTheme: () => {} };
+  }
 }
