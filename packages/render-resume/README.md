4201d2e54de8a336b92988f1c0d38f67b58a31f6
Matheus Belo <matheus_belo@id.uff.br>
2023-07-12 05:02:33 -0300

    Add Express server and deploy to beanstalk

---
+++ packages/render-resume/README.md
@@ -0,0 +1,3 @@
+# render-resume
+
+A simple Express.js app to render the resume using Puppeteer, because I couldn't get it to work on a Lambda function. (file size too large)
---
+++ packages/render-resume/index.ts
@@ -0,0 +1,103 @@
+import express, { Router, Request, Response } from 'express';
+import puppeteer from 'puppeteer';
+
+const RESUME_ENDPOINT = 'https://mkalo.dev/resume';
+
+const port = process.env.PORT || 3333;
+const app = express();
+const route = Router();
+
+async function renderResume(darkMode?: boolean) {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 950,
+    height: 950,
+  });
+
+  await page.goto(RESUME_ENDPOINT);
+
+  const htmlElement = await page.$('html');
+
+  if (!htmlElement) {
+    throw new Error('No html element found');
+  }
+
+  const boundingBox = await htmlElement.boundingBox();
+
+  if (!boundingBox) {
+    throw new Error('No bounding box found');
+  }
+
+  if (darkMode) await htmlElement.evaluate((el) => el.classList.add('dark'));
+
+  const buffer = await page.pdf({
+    width: boundingBox.width + 'px',
+    height: boundingBox.height + 50 + 'px',
+    printBackground: true,
+  });
+
+  await browser.close();
+
+  return buffer;
+}
+
+interface RenderedCache {
+  buffer: Buffer;
+  time: number;
+  type: 'dark' | 'light';
+}
+
+const cache: RenderedCache[] = [];
+
+async function getRenderedResume(darkMode?: boolean, expire = 1000 * 60 * 60) {
+  const cachedIndex = cache.findIndex(
+    (c) => c.type === (darkMode ? 'dark' : 'light')
+  );
+
+  if (cachedIndex !== -1) {
+    const cached = cache[cachedIndex];
+
+    if (Date.now() - cached.time < expire) {
+      return cached.buffer;
+    } else {
+      cache.splice(cachedIndex, 1);
+    }
+  }
+
+  const buffer = await renderResume(darkMode);
+
+  cache.push({
+    buffer,
+    time: Date.now(),
+    type: darkMode ? 'dark' : 'light',
+  });
+
+  return buffer;
+}
+
+route.get('/', async (req: Request, res: Response) => {
+  const darkMode = req.query.darkMode === 'true';
+  let buffer: Buffer;
+  try {
+    buffer = await getRenderedResume(darkMode);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'inline; filename=resume.pdf');
+  res.setHeader('Content-Length', buffer.length);
+  res.send(buffer);
+});
+
+app.use(route);
+
+app.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`);
+});
