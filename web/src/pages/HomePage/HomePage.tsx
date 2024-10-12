9481693edbcbcaea2300ed0686f4fb22c6d77718
Matheus Belo <matheus_belo@id.uff.br>
2024-10-12 20:50:04 -0300

    Set og image to profile picture

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,11 @@ import ResumePage from '../ResumePage/ResumePage';
 const HomePage = () => {
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
+      <MetaTags
+        title="Home"
+        description="Home page"
+        ogContentUrl="/profile.webp"
+      />
       <div className="flex items-center">
         <div className="mx-auto max-w-[950px] rounded lg:shadow-[0_1px_9px_#e8e8e8] lg:dark:shadow-[0_1px_9px_#1a1a1a]">
           <ResumePage showDownloadButton={true} />
